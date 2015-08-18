05efbf0c58e1cd2d0429d711e13d6c39cd615b14
David Morgan-Brown <david@morgan-brown.com>
2015-08-18 19:38:19 +0100

    Initial Commit

---
+++ swift-normal-distribution/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-normal-distribution
+//
+//  Created by David Morgan-Brown on 18/08/2015.
+//  Copyright (c) 2015 David Morgan-Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ swift-normal-distributionTests/swift_normal_distributionTests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_normal_distributionTests.swift
+//  swift-normal-distributionTests
+//
+//  Created by David Morgan-Brown on 18/08/2015.
+//  Copyright (c) 2015 David Morgan-Brown. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class swift_normal_distributionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
